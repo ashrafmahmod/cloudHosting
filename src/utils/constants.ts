8666f2742d50a984f38d5b57017eaba359c90866
ashrafmahmod <ashraf.mahmoud.mouhamed.mahmod@gmail.com>
2026-04-30 16:34:03 +0300

    new update

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,6 @@
 export const articlesPerPage = 6;
-// export const domain = "http://localhost:3000"
-export const domain = "https://cloud-hosting-khaki.vercel.app";
+const productionDomain = "https://cloud-hosting-khaki.vercel.app";
+const developmentDomain = "http://localhost:3000";
+
+export const domain =
+  process.env.NODE_ENV === "production" ? productionDomain : developmentDomain;
